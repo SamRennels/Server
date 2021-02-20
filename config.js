a8e56edc3ee6c2bf0127e23c9d27e4c4d3e65e7b
Sam Rennels <sam.rennels93@gmail.com>
2021-02-19 17:58:04 -0700

    week 4

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '264939248320563',
+        clientSecret: '26198190bac0681a27721e4480b7aebe'
+    }
 }
